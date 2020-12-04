c38a0ca12082bd74665ccb57117aef217e6bc40a
VictorRotha <Victor.Rotha@t-online.de>
2020-12-04 17:09:33 +0100

    Day 04 Part 02

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - [Day 01: Report Repair](https://github.com/VictorRotha/AdventOfCode2020/blob/main/src/days/Day01.java)
 - [Day 02: Password Philosophy](https://github.com/VictorRotha/AdventOfCode2020/blob/main/src/days/Day02.java)
 - [Day 03: Toboggan Trajectory](https://github.com/VictorRotha/AdventOfCode2020/blob/main/src/days/Day03.java)
+- [Day 04: Passport Processing](https://github.com/VictorRotha/AdventOfCode2020/blob/main/src/days/Day04.java)
--- src/days/Day04.java
+++ src/days/Day04.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Day04 {
@@ -12,15 +13,30 @@ public class Day04 {
         String path = "src/input/Day04_input.txt";
         ArrayList<HashMap<String, String>> input = readInput(path);
 
-        int valid = 0;
+        int valid1 = 0; int valid2 = 0;
+        String[] validEcls = {"amb", "blu","brn", "gry", "grn", "hzl", "oth"};
         for (HashMap<String, String> pass : input) {
             if (pass.size() == 8 || (pass.size() == 7 && !pass.containsKey("cid")))  {
-                valid += 1;
+                valid1 += 1;
+
+                String hgt = pass.get("hgt");
+                int hgtNumber = Integer.parseInt(hgt.substring(0,hgt.length()-2));
+                if ((Integer.parseInt(pass.get("byr")) < 1920 || Integer.parseInt(pass.get("byr")) > 2002)
+                    || (Integer.parseInt(pass.get("iyr")) < 2010 || Integer.parseInt(pass.get("iyr")) > 2020)
+                    || (Integer.parseInt(pass.get("eyr")) < 2020 || Integer.parseInt(pass.get("eyr")) > 2030)
+                    || (!pass.get("hcl").matches("#[a-f0-9]{6}"))
+                    || (!Arrays.asList(validEcls).contains(pass.get("ecl")))
+                    || (!pass.get("pid").matches("[0-9]{9}"))
+                    || (hgt.endsWith("cm") && (hgtNumber < 150 || hgtNumber > 193))
+                    || (hgt.endsWith("in") && (hgtNumber < 59 || hgtNumber > 76))
+                    || (!hgt.endsWith("in") && !hgt.endsWith("cm"))
+                ) continue;
+
+                valid2 ++;
             }
         }
-
-        System.out.printf("Part 01: %s of %s passports are valid\n", valid, input.size());
-
+        System.out.printf("Part 01: %s of %s passports are valid\n", valid1, input.size());
+        System.out.printf("Part 02: %s of %s passports are valid\n", valid2, input.size());
     }
 
     public static HashMap<String, String> getPassport(String entry) {
