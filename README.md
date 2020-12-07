188e9afc05f54788a9b4549987ca5dd527d36de9
VictorRotha <Victor.Rotha@t-online.de>
2020-12-07 11:48:14 +0100

    Day 07 Part 02

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 - [Day 04: Passport Processing](https://github.com/VictorRotha/AdventOfCode2020/blob/main/src/days/Day04.java)
 - [Day 05: Binary Boarding](https://github.com/VictorRotha/AdventOfCode2020/blob/main/src/days/Day05.java)
 - [Day 06: Custom Customs](https://github.com/VictorRotha/AdventOfCode2020/blob/main/src/days/Day06.java)
+- [Day 07: Handy Haversacks](https://github.com/VictorRotha/AdventOfCode2020/blob/main/src/days/Day07.java)
--- src/days/Day07.java
+++ src/days/Day07.java
@@ -3,7 +3,6 @@ package days;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
@@ -12,17 +11,19 @@ public class Day07 {
 
     public static void main(String[] args) {
         String path = "src/input/Day07_input.txt";
-        HashMap<String, ArrayList<String>> bags = readInput(path);
+        HashMap<String, HashMap<String, Integer>>  bags = readInput(path);
         String target = "shiny-gold";
         int count = 0;
         for (String bag :  bags.keySet()) {
             if (searchBags(bags, bag, target)) count++;
         }
+
         System.out.printf("%s bags can contain %s\n", count, target);
+        System.out.printf("1 shiny gold bag contains %s other bags\n", countBags(bags, target) - 1);
     }
 
-    public static HashMap<String, ArrayList<String>> readInput(String path) {
-        HashMap<String, ArrayList<String>> bags = new HashMap<>();
+    public static HashMap<String, HashMap<String, Integer>>  readInput(String path) {
+        HashMap<String, HashMap<String, Integer>>  bags = new HashMap<>();
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
             String line, key;
@@ -30,9 +31,9 @@ public class Day07 {
                 String[] s = line.split(" ");
                 key = String.join("-", Arrays.copyOfRange(s, 0, 2));
                 if (!Arrays.asList(s).contains("other")) {
-                    bags.put(key, new ArrayList<>());
+                    bags.put(key, new HashMap<>());
                     for (int i = 4; i < s.length; i += 4) {
-                        bags.get(key).add(String.join("-", Arrays.copyOfRange(s, i + 1, i + 3)));
+                        bags.get(key).put(String.join("-", Arrays.copyOfRange(s, i + 1, i + 3)), Integer.parseInt(s[i]));
                     }
                 }
                 }
@@ -43,15 +44,15 @@ public class Day07 {
         return bags;
     }
 
-    public static boolean searchBags(HashMap<String, ArrayList<String>> bags, String bag, String target) {
+    public static boolean searchBags(HashMap<String, HashMap<String, Integer>> bags, String bag, String target) {
         if (!bags.containsKey(bag)) {
             return false;
         } else {
-            ArrayList<String> sbags = bags.get(bag);
-            if (sbags.contains(target)) {
+            HashMap<String, Integer> sbags = bags.get(bag);
+            if (sbags.containsKey(target)) {
                 return true;
             } else {
-                for (String sbag : sbags) {
+                for (String sbag : sbags.keySet()) {
                     if (searchBags(bags, sbag, target)) {
                         return true;
                     }
@@ -59,6 +60,21 @@ public class Day07 {
             }
         }
         return false;
+    }
+
+    public static int countBags(HashMap<String, HashMap<String, Integer>> bags, String bag) {
+        int result = 0;
+        if (!bags.containsKey(bag)) {
+            return 1;
+        }
+        result ++;
+        HashMap<String, Integer> sbags = bags.get(bag);
+        for (String sbag : sbags.keySet()) {
+            for (int i = 0; i < sbags.get(sbag); i++) {
+                result += countBags(bags, sbag);
+            }
+        }
+        return result;
     }
 
 }
