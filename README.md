8ebcd9c6c64f2877b80981b7e600524dbd7a6b39
VictorRotha <Victor.Rotha@t-online.de>
2020-12-09 18:38:53 +0100

    Day 09 Part 02

--- README.md
+++ README.md
@@ -10,5 +10,6 @@
 - [Day 06: Custom Customs](https://github.com/VictorRotha/AdventOfCode2020/blob/main/src/days/Day06.java)
 - [Day 07: Handy Haversacks](https://github.com/VictorRotha/AdventOfCode2020/blob/main/src/days/Day07.java)
 - [Day 08: Handheld Halting](https://github.com/VictorRotha/AdventOfCode2020/blob/main/src/days/Day08.java)
+- [Day 09: Encoding Error](https://github.com/VictorRotha/AdventOfCode2020/blob/main/src/days/Day09.java)
 
 (Removed all input data from Repository)
--- src/days/Day09.java
+++ src/days/Day09.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Day09 {
 
@@ -11,14 +12,16 @@ public class Day09 {
         String path = "src/input/Day09_input.txt";
         ArrayList<Long> input = readInput(path);
 
+        long inValidNumber = 0;
         int preamble = 25;
         for (int i = preamble; i < input.size(); i++) {
             if (!isValid(input, i, preamble)) {
-                System.out.printf("First invalid number is %s as index %s\n", input.get(i), i);
+                inValidNumber = input.get(i);
                 break;
             }
         }
-
+        System.out.printf("First invalid number is %s \n", inValidNumber);
+        System.out.printf("The encryption weakness is %s \n", findSet(input, inValidNumber));
     }
 
     public static ArrayList<Long> readInput(String path) {
@@ -43,4 +46,30 @@ public class Day09 {
         }
         return false;
     }
+
+    public static Long findSet(ArrayList<Long> input, long inValidNumber) {
+        long sum, minValue, maxValue;
+        for (int i = 0; i < input.size(); i++) {
+            sum = 0;
+            int j = 0;
+            if (input.get(i) < inValidNumber) {
+                while (sum < inValidNumber) {
+                    sum += input.get(i + j);
+                    if (sum == inValidNumber) {
+                        ArrayList<Long> result = new ArrayList<>();
+                        for (int k = i; k <= i + j; k++) {
+                            result.add(input.get(k));
+                        }
+                        minValue = Collections.min(result);
+                        maxValue = Collections.max(result);
+                        return minValue + maxValue;
+                    }
+                    j++;
+                }
+            }
+        }
+
+        return 0L;
+    }
+
 }
