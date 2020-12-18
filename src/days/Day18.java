1c162687f4440da935990541787c6d7871f3cc3b
VictorRotha <Victor.Rotha@t-online.de>
2020-12-18 17:22:14 +0100

    Day 18 Part 01

---
+++ src/days/Day18.java
@@ -0,0 +1,103 @@
+package days;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Day18 {
+
+    public static void main(String[] args) {
+        String path = "src/input/Day18_input.txt";
+        System.out.printf("Part 01 sum is %s\n", readInput(path));
+    }
+
+    public static long readInput(String path) {
+        long result = 0;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String line;
+            while ((line = br.readLine()) != null) {
+                result += runExpression(line.strip());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static long runExpression(String expr) {
+        long result = 0;
+        ArrayList<String[]> split = splitExpression(expr);
+
+        for (String[] e : split) {
+            long p;
+            if (e[0].contains("+") || e[0].contains("*")) {
+                p = runExpression(e[0]);
+            } else {
+                p = Integer.parseInt(e[0]);
+            }
+            result = (e[1].equals("*")) ? result * p : result + p;
+        }
+        return result;
+    }
+
+    public static ArrayList<String[]> splitExpression(String input) {
+        int idx = 0, nxtIdx;
+        int nxtOpen, nxtClosed, oPara;
+        String op = "+";
+        ArrayList<String[]> result = new ArrayList<>();
+
+        while (idx < input.length()) {
+            char c = input.charAt(idx);
+            if (c == ' ') {
+                idx++;
+                continue;
+            }
+
+            if (c == '(') {
+                nxtIdx = idx + 1;
+                oPara = 1;
+                while (true) {
+                    nxtOpen = input.indexOf('(', nxtIdx);
+                    nxtClosed = input.indexOf(')', nxtIdx);
+                    if (nxtClosed < nxtOpen || nxtOpen == -1) {
+                        oPara--;
+                        nxtIdx = nxtClosed + 1;
+                    } else {
+                        oPara++;
+                        nxtIdx = nxtOpen + 1;
+                    }
+
+                    if (oPara == 0) {
+                        String sub = input.substring(idx+1, nxtIdx-1);
+                        result.add(new String[]{sub, op});
+                        idx = nxtIdx;
+                        break;
+                    }
+                }
+
+            } else if (c == '+' || c == '*') {
+                op = c + "";
+                idx += 1;
+            } else {
+                nxtIdx = input.indexOf(" ", idx + 1);
+                if (nxtIdx == -1) {
+                    String sub = input.substring(idx);
+                    result.add(new String[]{sub, op});
+                    break;
+                }
+                String sub = input.substring(idx, nxtIdx);
+                result.add(new String[]{sub, op});
+                idx = nxtIdx;
+            }
+        }
+        return result;
+    }
+
+
+
+
+
+}
